90920b84d190a0fe1f06c1a92d8b1032baefd790
pb2019-src <ahmadalmanassra@gmail.com>
2020-07-23 01:45:16 +0800

    Wait for instances to stop and start during snapshot process

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -41,22 +41,6 @@ def list_snapshots(project):
                 )))
     return
 
-@instances.command('snapshot',
-    help="Create snapshots of all volumes")
-@click.option("--project",default=None,
-    help="Only instances for project(tag Project:<name>)")
-def create_snapshots(project):
-    "Create snapshots for EC2 instances"
-
-    instances=filter_instances(project)
-
-    for i in instances:
-        i.stop()
-        for v in i.volumes.all():
-            print("Creating snapshot of {0}.formate(vi.id))
-            v.create_snapshot(Description="Created by SnapshotAlyzer 30000)
-
-    return __
 
 @cli.group('volumes')
 def volumes():
@@ -86,6 +70,31 @@ def list_volumes(project):
 @cli.group("instances")
 def instances():
     """Commmands for instances"""
+
+@instances.command('snapshot',
+    help="Create snapshots of all volumes")
+@click.option("--project",default=None,
+    help="Only instances for project(tag Project:<name>)")
+def create_snapshots(project):
+    "Create snapshots for EC2 instances"
+
+    instances=filter_instances(project)
+
+    for i in instances:
+        print("Stopping {0}...".format(i.id))
+        i.stop()
+        i.wait_until_stopped()
+        for v in i.volumes.all():
+            print("  Creating snapshot of {0}".format(v.id))
+            v.create_snapshot(Description="Created by SnapshotAlyzer 30000")
+        print("Starting {0}...".format(i.id))
+        i.start()
+        i.wait_until_running()
+
+    print("Job's done!")
+
+    return
+
 
 @instances.command('list')
 @click.option('--project',default=None,
